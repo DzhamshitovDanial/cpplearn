9914d83605be00db81e1cba7d52862cbd5d19d9f
Dzhamshitov Danial <finmotive7@gmail.com>
2026-08-29 18:06:41 +0500

    updated double and triple and comment

--- chaponeprac.cpp
+++ chaponeprac.cpp
@@ -5,7 +5,15 @@
 int main()
 {
     std::cout << "Enter a number" << "\n";
+
     int n {};
     std::cin >> n;
-    std::cout << "Result:" << n*2;
+
+    int doublen {n*2};
+    int triplen {n*3};
+
+    std::cout << "double:" << doublen << "\n";
+    std::cout << "triple:" << triplen;
 }
+ /* today is 29.08.2026 learning c++ by learncpp and listening B's Revenge
+    Time 18:06*/
